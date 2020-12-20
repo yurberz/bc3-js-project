34c74f600d9686d3a20a10ffda3c9ac674d4f242
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-12-20 13:09:41 +0200

    added responsive markup and styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import '../node_modules/normalize.css/normalize.css';
 import './styles.css';
-import './components/hero/slider-item/slider-item.css';
-
+import './components/hero/slider-item/index';
