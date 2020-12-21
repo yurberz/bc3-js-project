b6216d8cd85a477a11feb2a232a50e8886593a9b
Igor Vasylytsya <vasylytsya.igor@gmail.com>
2020-12-21 17:10:19 +0200

    seach igror and change index.html

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,1 +1,15 @@
-const test = [];
+const mainInput = document.getElementById('search-input');
+console.dir(mainInput);
+// console.log('hello');
+
+mainInput.addEventListener('input', e => {
+  console.log(e.target.value);
+});
+
+// function onSearch(e) {
+//   // const query = e.value;
+//   //   preventDefault();
+
+//   console.dir(e.target);
+//   // console.log(query);
+// }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import './components/card/index';
-import './components/loader/index';
 import '../node_modules/normalize.css/normalize.css';
 import './styles.css';
-import './components/auth/auth'
+import './components/card/index';
+import './components/loader/index';
+import './components/search/index';
 import './components/footer/styles.js';
 import './components/hero/slider-item/slider-item';
 import './components/header/js/header';
 import './components/modal/modalBackDrop.js';
+// import './components/auth/auth';
