cb2ab31dfe0b46820531cfc63882c646b7bab925
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-12-22 00:17:37 +0200

    fixed modal problems

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import './components/card/index';
 import './components/loader/index';
 import './components/auth/auth';
 import './components/modal/modalBackDrop.js';
-import './components/hero/slider-item/slider-item'
 import './components/developers/js/index';
 import './components/developers/modal-window/dev-modal';
 import './components/developers/modal-window/dev-styles.css';
