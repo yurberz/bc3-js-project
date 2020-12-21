4f60da13aaa39cb097379f6f453da448371a7ef4
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-12-21 21:26:29 +0200

    added random items from api and fixed styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import './components/card/index';
-import './components/loader/index';
 import '../node_modules/normalize.css/normalize.css';
 import './styles.css';
-import './components/auth/auth'
-import './components/footer/styles.js';
-import './components/hero/slider-item/slider-item';
 import './components/header/js/header';
+import './components/hero/slider-item/slider-item';
+import './components/footer/styles.js';
+import './components/card/index';
+import './components/loader/index';
+// import './components/auth/auth'
 import './components/modal/modalBackDrop.js';
+import './components/hero/slider-item/slider-item'
